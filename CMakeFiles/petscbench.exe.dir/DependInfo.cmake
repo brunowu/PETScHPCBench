4b6370353f807f06012a4bb4b776e42306326d4b
brunowu <xinzhe.wu1990@gmail.com>
2018-08-31 14:13:28 +0200

    change makefile to cmake

---
+++ CMakeFiles/petscbench.exe.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/xwu/phd_xwu/PETScHPCBench/hpc_petsc_bench.c" "/Users/xwu/phd_xwu/PETScHPCBench/CMakeFiles/petscbench.exe.dir/hpc_petsc_bench.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "__INSDIR__="
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/Users/xwu/software/petsc-3.8.3/include"
+  "/Users/xwu/software/petsc-3.8.3/arch-darwin-c-debug/include"
+  "/opt/X11/include"
+  "/Users/xwu/software/openmp/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/petscbench.exe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/petscbench.exe.dir/hpc_petsc_bench.c.o"
+  "petscbench.exe.pdb"
+  "petscbench.exe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/petscbench.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
